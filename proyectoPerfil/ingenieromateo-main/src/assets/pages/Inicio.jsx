6fc22baa9abb198fdf079e0a697f039bbfb9cd96
TeoZapata <masalazarza@unal.edu.co>
2024-09-27 11:50:04 -0500

    WALLPAPPER PR

--- proyectoPerfil/ingenieromateo-main/src/assets/pages/Inicio.jsx
+++ proyectoPerfil/ingenieromateo-main/src/assets/pages/Inicio.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import Proyecto from "./Proyectos";
-
+import { Wallpaper } from "lucide-react";
 export default function Inicio() {
   return (
     <section id="title" className="General-Container ">
       <h1 className="perfil-title">Hola, Soy Mateo Salazar Zapata</h1>
-      
     </section>
   );
 }
